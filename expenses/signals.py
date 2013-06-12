a367f2aeda790afc0ef7815909026157ff91cda9
Nicolas Bouliane <contact@nicolasbouliane.com>
2013-06-12 15:36:37 -0400

    Create a default group when a user registers

---
+++ expenses/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from expenses.models import Group
+from django.utils.translation import ugettext as _
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_initial_group(sender, instance, signal, *args, **kwargs):
+    """
+    When a user is created, add a default group
+    """
+    group = Group(name=_('Personal expenses'))
+    group.save()
+    group.users.add(instance)
+    group.save()
