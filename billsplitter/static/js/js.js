af1309ad4a6b61d64149443e22ead1d5f3143836
Nicolas Bouliane <contact@nicolasbouliane.com>
2013-07-01 15:32:17 -0400

    Fixed multiple display bugs on mobile devices, made tables more readable on iPhone

--- billsplitter/static/js/js.js
+++ billsplitter/static/js/js.js
@@ -1,15 +1,24 @@
 $(document).ready(function(){
-    //Only use the standard date field on touchscreen devices
+    //CORRECTIONS
     if (screen.width > 768 && screen.height > 768) {
+        //Only use the standard date field on touchscreen devices
         var datepickers = $('input[type="date"]');
         try{
             datepickers.prop('type','text');
         }
         catch(e){
             //Expected to fail with no consequences in IE
-            console.log('Could not change input type from date to text')
+            console.log('Could not change input type from date to text');
         }
         datepickers.datepicker({dateFormat: "yy-mm-dd", changeYear: true, yearRange: "-2:+5"});
+
+    } else {
+        //Elements that take 100% of the viewport on mobile devices
+        //This simply compensates for the 20px padding on the body
+        $('.full-width').css({
+            'width':'+=40',
+            'margin-left':'-20px',
+        })
     }
 
     //Numeric field
