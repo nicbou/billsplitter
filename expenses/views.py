fc093fdfe3ee499a3394fa9fd2fe052130f2669f
Nicolas Bouliane <contact@nicolasbouliane.com>
2013-07-21 19:46:35 -0400

    Implemented pagination in the expense list

--- expenses/views.py
+++ expenses/views.py
@@ -20,11 +20,13 @@ class Home(TemplateView):
             return redirect('group_list')
         return super(Home, self).dispatch(request, *args, **kwargs)
 
+
 class GroupList(LoginRequiredViewMixin, ListView):
     model = Group
 
     def get_queryset(self):
         return self.request.user.expense_groups.all()
+
 
 class GroupCreate(LoginRequiredViewMixin, CreateView):
     model = Group
@@ -35,9 +37,11 @@ class GroupCreate(LoginRequiredViewMixin, CreateView):
         self.object.users.add(self.request.user)
         return super(GroupCreate, self).form_valid(form) 
 
+
 class GroupUpdate(LoginRequiredViewMixin, UpdateView):
     model = Group
     form_class = GroupForm
+
 
 class GroupDelete(LoginRequiredViewMixin, DeleteView):
     model = Group
@@ -67,6 +71,7 @@ class InviteCreate(LoginRequiredViewMixin, DetailView):
     def get_queryset(self):
         return self.request.user.expense_groups.all()
 
+
 class InviteDetail(LoginRequiredViewMixin, DetailView):
     """
     Shows an invitation, provided that you have a valid invite code
@@ -80,6 +85,7 @@ class InviteDetail(LoginRequiredViewMixin, DetailView):
             return group
         else:
             raise Http404
+
 
 class InviteAccept(LoginRequiredViewMixin, RedirectView):
     """
@@ -118,13 +124,16 @@ class ExpenseViewMixin(object):
     def get_success_url(self):
         return reverse_lazy('expense_list',kwargs={'group':self.kwargs['group']})
 
+
 class ExpenseList(ExpenseViewMixin, LoginRequiredViewMixin, ListView):
+    paginate_by = 20
     def get_context_data(self, **kwargs):
         group = self.request.user.expense_groups.get(pk=self.kwargs['group'])
         context = super(ExpenseList, self).get_context_data(**kwargs)
         context['group'] = group
         context['users'] = group.users_with_totals(self.request.user)
         return context
+
 
 class ExpenseCreate(ExpenseViewMixin, LoginRequiredViewMixin, CreateView):
     form_class = ExpenseForm
@@ -146,8 +155,10 @@ class ExpenseCreate(ExpenseViewMixin, LoginRequiredViewMixin, CreateView):
 
         return super(ExpenseCreate, self).form_valid(form)
 
+
 class ExpenseUpdate(ExpenseViewMixin, LoginRequiredViewMixin, UpdateView):
     form_class = ExpenseForm
+
 
 class ExpenseDelete(ExpenseViewMixin, LoginRequiredViewMixin, DeleteView):
     pass
@@ -167,6 +178,7 @@ class RefundViewMixin(object):
             
     def get_success_url(self):
         return reverse_lazy('expense_list',kwargs={'group':self.kwargs['group']})
+
 
 class RefundCreate(RefundViewMixin, LoginRequiredViewMixin, CreateView):
     form_class = RefundForm
@@ -204,6 +216,7 @@ class RefundCreate(RefundViewMixin, LoginRequiredViewMixin, CreateView):
         form.instance.save()
 
         return super(RefundCreate, self).form_valid(form)
+
 
 class RefundDelete(RefundViewMixin, LoginRequiredViewMixin, DeleteView):
     pass
